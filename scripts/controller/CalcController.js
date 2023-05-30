bd49e790594bab9554b8542a15de8f1c6c748989
Misael Kosmalski <misaelkosmalski@gmail.com>
2023-05-30 00:52:45 -0300

    Percent button

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -28,10 +28,12 @@ class CalcController{
 
     clearAll(){
         this._operation = [];
+        this.setLastNumberToDisplay();
     }
 
     cancelEntry(){
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
     getLastOperation(){
@@ -55,12 +57,19 @@ class CalcController{
     }
 
     calc(){
-        let last = this._operation.pop();
-
+        let last = "";
+        if (this._operation.length > 3){
+            last = this._operation.pop();
+        }
         let result = eval(this._operation.join("")); 
 
-        this._operation = [result, last];
-
+        if (last == "%"){
+            result /= 100;
+            this._operation = [result];
+        } else {
+        this._operation = [result];
+        }
+        if(last) this._operation.push(last);
         this.setLastNumberToDisplay();
     }
 
@@ -73,6 +82,8 @@ class CalcController{
                 break;
             }
         }
+        if (!lastNumber) lastNumber = 0;
+
         this.displayCalc = lastNumber;
     }
 
@@ -126,7 +137,7 @@ class CalcController{
                 this.addOperation("%");
             break;
             case "igual":
-
+                this.calc();
             break;
             case "ponto": 
                 this.addOperation(".");
