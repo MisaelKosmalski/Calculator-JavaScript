1cfb44585b0cddb47759e79e57d85528a918176d
Misael Kosmalski <misaelkosmalski@gmail.com>
2023-06-01 16:35:56 -0300

    Appying audio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor(){
 
+        this._audio = new Audio("click.mp3");
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -60,11 +62,40 @@ class CalcController {
 
         this.pasteFromClipboard();
 
+        document.querySelectorAll(".btn-ac").forEach(btn => {
+
+            btn.addEventListener("dblclick", e => {
+                
+                this.toggleAudio();
+
+            });
+
+    });
+
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if(this._audioOnOff){
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+
+        }
+
     }
 
     initKeyboard(){
 
         document.addEventListener("keyup", e => {
+
+            this.playAudio();
 
             switch (e.key) {
 
@@ -320,6 +351,8 @@ class CalcController {
     }
 
     execBtn(value){
+
+        this.playAudio();
 
         switch (value) {
 
